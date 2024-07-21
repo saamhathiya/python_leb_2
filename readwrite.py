27db9994961ee5eabbcb8738e95fbe0c0f4092eb
saamhathiya <bca2022saam1724@tnraocollege.org>
2024-07-21 22:31:33 +0530

    Create readwrite.py

---
+++ readwrite.py
@@ -0,0 +1,7 @@
+# Open a file in write mode and write content
+with open("example.txt", "w") as file:
+    file.write("Hello, this is some text.")
+
+# Open a file in write mode and write content
+with open("example.txt", "w") as file:
+    file.write("Hello, this is some text.")
