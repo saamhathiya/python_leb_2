44fbeacb7dcb3ce0923f1bd2fe56fa3fdffd5c95
saamhathiya <bca2022saam1724@tnraocollege.org>
2024-07-21 22:15:46 +0530

    Update reversing.py

--- reversing.py
+++ reversing.py
@@ -4,3 +4,6 @@ reversed_list = original_list[::-1]
 
 print("Original List:", original_list)
 print("Reversed List:", reversed_list)
+
+output :-
+![Screenshot 2024-07-21 221104](https://github.com/user-attachments/assets/5a2ecf4b-72a5-4b41-a151-910586da8f0d)
