bcdbb83c96510bee32466e84f2ba57fcf5d80930
saamhathiya <bca2022saam1724@tnraocollege.org>
2024-07-21 22:03:26 +0530

    Update length.py

--- length.py
+++ length.py
@@ -3,3 +3,6 @@ my_list = [10, 20, 30, 40, 50]
 list_length = len(my_list)
 
 print("Length of the list:", list_length)
+
+output :-
+![Screenshot 2024-07-21 220052](https://github.com/user-attachments/assets/66c2fe73-734e-4301-9f80-c1704ffbd5d7)
