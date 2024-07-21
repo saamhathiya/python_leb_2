7156c1fbd97692e30c876bf7c403cb24d6490f3b
saamhathiya <bca2022saam1724@tnraocollege.org>
2024-07-21 21:43:33 +0530

    Update interchange.py

--- interchange.py
+++ interchange.py
@@ -5,3 +5,6 @@ def interchange_first_last(input_list):
 input_list = [12, 35, 9, 56, 24]
 output_list = interchange_first_last(input_list)
 print(output_list)
+
+output :-
+![Screenshot 2024-07-21 214220](https://github.com/user-attachments/assets/c3a3fb2f-4000-42a5-ab8c-564b33177477)
